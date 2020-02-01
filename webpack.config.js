fc28bdf4a8a897b51354dbf66fd0f41b29b0d8ef
ToavinaMendrika <mendrika.toavina.andriantsivolosy@esti.mg>
2020-02-01 22:40:34 +0300

    build task minify css and js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,51 @@
 const path = require('path')
+const fs = require('fs')
+
+/**
+ *  Webpack plugins
+ */
 const MiniCssExtractPlugin  = require('mini-css-extract-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+
+/**
+ * Environment
+ */
+const __env = process.env.NODE_ENV
+const __hmr = false
 
 const paths = {
     scss: './src/scss/styles.scss',
     js: './src/js/app.js',
-    outputDir: path.resolve(__dirname, 'public')
+    publicPath: path.resolve(__dirname, 'public'),
+    outputDir: path.resolve(__dirname, 'public/assets')
 }
+
+
+
+let utils = {
+
+    /**
+     * Clean directory
+     * @param {striny} directory path to directory to clean up
+     */
+    cleanup(directory) {
+        if (fs.existsSync(directory)) {
+            fs.readdirSync(directory).forEach((file, index) => {
+                const curPath = path.join(directory, file);
+                if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                    this.cleanup(curPath);
+                } else { // delete file
+                    fs.unlinkSync(curPath);
+                }
+            });
+            fs.rmdirSync(directory);
+        }
+    }
+}
+
+console.log('clean up ...')
+utils.cleanup(paths.outputDir)
+console.log('generate assets ...')
 
 module.exports = {
     mode: 'development',
@@ -13,10 +53,10 @@ module.exports = {
     entry: [paths.scss, paths.js],
     devtool: 'source-map',
     output: {
-        publicPath: 'public',
+        publicPath: 'public/assets/',
         path: paths.outputDir,
         sourceMapFilename: '[file].map[query]',
-        filename: 'js/[name].js'
+        filename: __env === 'dev'? 'js/[name].js' :  'js/[name]-[hash:8].js'
     },
 
     module: {
@@ -26,7 +66,7 @@ module.exports = {
                 use: [
                     'style-loader', 
                     MiniCssExtractPlugin.loader ,
-                    'css-loader?sourceMap', 'postcss-loader', 'sass-loader?sourceMap',
+                    'css-loader', 'postcss-loader', 'sass-loader',
                 ]
             },
             {
@@ -41,7 +81,9 @@ module.exports = {
                     {
                         loader: 'file-loader',
                         options: {
-                            name: 'img/[name].[ext]'
+                            name: '[name].[ext]',
+                            outputPath: 'img',
+                            publicPath: '/assets/img',
                         }
                     }
                 ]
@@ -52,9 +94,17 @@ module.exports = {
     plugins: [
         new MiniCssExtractPlugin (
             {
-                filename: "css/[name].css",
+                filename:  __env === 'dev'? 'css/[name].css' :  'css/[name]-[hash:8].css',
                 chunkFilename: "[id].css"
             }
-        )
+        ),
+        new ManifestPlugin({
+            seed: {
+                env: __env,
+                hmr: __hmr
+            },
+            fileName: 'assets.json',
+            filter: file => file.path.match(/.*.(css|js|ttf|woff2?)$/)
+        })
     ]
 }
